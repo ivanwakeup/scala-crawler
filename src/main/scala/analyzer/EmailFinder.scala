250bac551af34bfba5b7c24512fca85c7591c453
ivanwakeup <ivan9miller@gmail.com>
2019-05-10 19:34:59 -0600

    use internal state for email finder

--- src/main/scala/analyzer/EmailFinder.scala
+++ src/main/scala/analyzer/EmailFinder.scala
@@ -2,19 +2,19 @@ package analyzer
 import akka.actor.Props
 import akka.util.ByteString
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
 import scala.concurrent.Future
 
 class EmailFinder extends BaseAnalyzer {
 
+  var emailSet: mutable.TreeSet[String] = scala.collection.mutable.TreeSet()
+
   override def analyze(bytes: ByteString): Future[Unit] = {
-    Future {
-      val result = ListBuffer[String]()
-      val emails = EmailFinder.EMAIL_REGEX.findAllIn(bytes.utf8String)
-      while(emails.hasNext) {
-        result.append(emails.next())
-      }
+    val emails = EmailFinder.EMAIL_REGEX.findAllIn(bytes.utf8String)
+    while(emails.hasNext) {
+      emailSet += emails.next()
     }
+    Future.successful()
   }
 }
 
