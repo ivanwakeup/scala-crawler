4e99370bbdd532f8e7e005baef75d687fca912d1
ivanwakeup <ivan9miller@gmail.com>
2019-07-18 09:56:02 -0600

    update accumulator, still trying to figure out how to get kafka stream to run appropriately

--- src/main/scala/analyzer/HtmlAccumulator.scala
+++ src/main/scala/analyzer/HtmlAccumulator.scala
@@ -1,10 +1,11 @@
 package analyzer
 
+import akka.{Done, NotUsed}
 import akka.actor.{ActorSystem, Props}
-import akka.kafka.ProducerSettings
+import akka.kafka.{ProducerMessage, ProducerSettings}
 import akka.kafka.scaladsl.Producer
 import akka.stream.{ActorMaterializer, OverflowStrategy}
-import akka.stream.scaladsl.{Keep, Source}
+import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
 import akka.util.ByteString
 import app.main.bootstrapServers
 import com.typesafe.config.ConfigFactory
@@ -15,8 +16,9 @@ import scala.concurrent.Future
 
 class HtmlAccumulator(system: ActorSystem) extends BaseAnalyzer {
 
-
   implicit val mat = ActorMaterializer()(system)
+
+  println("accumulator started")
 
   val config = ConfigFactory.load().getConfig("akka.kafka.producer")
   val producerSettings =
@@ -26,11 +28,11 @@ class HtmlAccumulator(system: ActorSystem) extends BaseAnalyzer {
   val q = Source.queue[String](100, OverflowStrategy.backpressure)
     .map {ele => new ProducerRecord[String, String](metadata.url, ele)}
     .recover({case e => throw e})
-    .toMat(Producer.plainSink(producerSettings))(Keep.left)
+    .to(Producer.plainSink(producerSettings))
     .run()
 
   override def analyze(bytes: ByteString): Future[Unit] = {
-    q.offer(bytes.utf8String).flatMap(_ => Future.successful())
+    q.offer(bytes.utf8String).flatMap(res  => {println(res); Future.successful()})
   }
 
 }
